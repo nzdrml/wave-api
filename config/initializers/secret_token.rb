7be2f2f129f4a16ed976b90924f9438f983ee494
Enzo Doromal <enzodoromal@gmail.com>
2016-06-05 18:21:32 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+WaveApi::Application.config.secret_token = '13c534fde96bac4161bb53c389a8ca7bb5d27953206131e15e207704d975b7d13c35617331809952da8427869eea3d851552cce5be2703547a918260354fe288'

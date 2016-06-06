af5d31547bd3d8cf68879c1ed36502b3d4446615
Enzo Doromal <enzodoromal@gmail.com>
2016-06-06 15:50:45 +0800

    Add user create route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   root 'application#index'
 
-  resources :users, :only => [:index, :show]
+  resources :users, :only => [:index, :show, :create]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

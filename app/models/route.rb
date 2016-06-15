58fed490b4f9ce89bb037717b699d5f40c7578c6
Enzo Doromal <enzodoromal@gmail.com>
2016-06-16 04:52:53 +0800

    Create models for points and routes. Setup relationships

---
+++ app/models/route.rb
@@ -0,0 +1,8 @@
+class Route < ActiveRecord::Base
+
+  belongs_to :origin, :foreign_key => 'origin_id', :class_name => 'Point'
+  belongs_to :destination,
+    :foreign_key => 'destination_id',
+    :class_name => 'Point'
+    
+end

14c9088d06b0739d42ff1cfdad89438b15599ddf
Enzo Doromal <enzodoromal@gmail.com>
2016-07-11 18:04:59 +0800

    Adjust relationships

--- app/models/booking.rb
+++
@@ -1,9 +0,0 @@
-class Booking < ActiveRecord::Base
-
-  belongs_to :origin, :foreign_key => 'origin_id', :class_name => 'Point'
-  belongs_to :destination,
-    :foreign_key => 'destination_id',
-    :class_name => 'Point'
-  belongs_to :user
-
-end
